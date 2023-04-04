ff4219a322facd71a6650e90cfc5badf1b6e79d5
Jelson9854 <Jelson9854@vt.edu>
2023-04-04 18:12:28 -0400

    Created a sample Flask app with OpenAI integration

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = "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"
+
+user = "How do I get good grades in Software Development"
+
+@app.route("/")
+def openAI():
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=user,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+)
+    print(response)
+    output = response['choices'][0]['text'].split("\n")
+    return render_template("index.html", responses=output, user=user)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
