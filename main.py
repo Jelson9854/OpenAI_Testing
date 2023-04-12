a485c1765695d15f65c081a6680f90ff69091cba
Jelson9854 <Jelson9854@vt.edu>
2023-04-12 14:37:45 -0400

    Added input feature. Style changes

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 openai.api_key = key.OPENAI_API_KEY
 
-user = "Name 5 animals"
-
 @app.route("/", methods=("GET", "POST"))
-def openAI():
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=user,
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-)
-    output = response['choices'][0]['text'].split("\n")
-    return render_template("index.html", responses=output, user=user)
+def index():
+    if request.method=="POST":
+        user = request.form["user"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=user,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+    
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
 
 
 if __name__=="__main__":
